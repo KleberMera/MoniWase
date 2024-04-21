2060cf0eae3b5af8f4c9682eafc64563e9d6f932
Kleber Mera <klebermera2016@gmail.com>
2024-04-20 23:07:17 -0500

    cambios en register

--- src/app/paginas/register/register.page.ts
+++ src/app/paginas/register/register.page.ts
@@ -15,6 +15,7 @@ export class RegisterPage implements OnInit {
   form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
+    name: new FormControl('', [Validators.required, Validators.email]),
   });
 
   firebaseSvc = inject(FirebaseService);
