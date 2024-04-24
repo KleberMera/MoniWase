befb9aa957962b0333b7592df15c9bdd443ef279
KleberMera <klebermera2016@gmail.com>
2024-04-24 11:17:29 -0500

    g

--- src/app/paginas/graficos/graficos.module.ts
+++ src/app/paginas/graficos/graficos.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { GraficosPageRoutingModule } from './graficos-routing.module';
 
 import { GraficosPage } from './graficos.page';
+import { ComponentesModule } from 'src/app/componentes/componentes.module';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { GraficosPage } from './graficos.page';
     FormsModule,
     IonicModule,
     GraficosPageRoutingModule,
+    ComponentesModule
 
   ],
   declarations: [GraficosPage]
